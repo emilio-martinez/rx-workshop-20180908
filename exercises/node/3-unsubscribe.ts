af99713642d24d970faaf64bab2df17230c6f825
Emilio Martinez <e.marticor@gmail.com>
2018-09-08 08:48:22 -0700

    feat: exercise 03

--- exercises/node/3-unsubscribe.ts
+++ exercises/node/3-unsubscribe.ts
@@ -3,3 +3,12 @@ import { data$ } from './fixtures/1-data';
 
 // TODO: Get the subscription and unsubscribe it after 1 second
 
+const subscription = data$.subscribe({
+  next(v) { console.log(v) },
+  complete() { console.log('done') }
+})
+
+setTimeout(() => {
+  console.log('1 second passed, goodbye');
+  subscription.unsubscribe();
+}, 1000)
