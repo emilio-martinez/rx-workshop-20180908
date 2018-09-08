1ef8d1a6e5dc9c637a1bdfb9b7f5af2037865e58
Emilio Martinez <e.marticor@gmail.com>
2018-09-08 08:43:56 -0700

    feat: exercise 02

--- exercises/node/2-subscribe-with-observer.ts
+++ exercises/node/2-subscribe-with-observer.ts
@@ -2,3 +2,15 @@
 import { data$ } from './fixtures/1-data';
 
 // TODO: create an observer to subscribe to `data$` and log out all values.
+
+// Sometimes better for legibility
+data$.subscribe({
+  next: (v) => console.log(v),
+  error: (err) => console.error(err),
+  complete: () => console.log('done')
+});
+
+// Also, easier to use less
+data$.subscribe({
+  complete: () => console.log('done')
+});
