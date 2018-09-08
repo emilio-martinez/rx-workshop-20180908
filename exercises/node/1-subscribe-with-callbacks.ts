3cf4dc671e4c09df5722abb5938d1324e7315438
Emilio Martinez <e.marticor@gmail.com>
2018-09-08 08:41:38 -0700

    feat: exercise 01

--- exercises/node/1-subscribe-with-callbacks.ts
+++ exercises/node/1-subscribe-with-callbacks.ts
@@ -4,3 +4,8 @@ import { data$ } from './fixtures/1-data';
 // TODO: Subscribe to `data$` using three callbacks and log out all values to console.
 // be sure to log errors and completions as well.
 
+data$.subscribe(
+  v => console.log(v),
+  null,
+  () => console.log('done')
+);
