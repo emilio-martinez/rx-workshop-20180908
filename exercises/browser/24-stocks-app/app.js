ad2d820576345c9bd3c85caa5d1746f00e731a1a
Emilio Martinez <e.marticor@gmail.com>
2018-09-08 16:29:25 -0700

    feat: exercise 24

--- exercises/browser/24-stocks-app/app.js
+++ exercises/browser/24-stocks-app/app.js
@@ -1,6 +1,9 @@
 (function () {
   // NOTE: Setup ---------------------------------------------
-
+  const { fromEvent, Observable, timer } = rxjs;
+  const { debounceTime, map, switchMap, tap, retryWhen, filter } = rxjs.operators;
+  const { ajax } = rxjs.ajax;
+  const { WebSocketSubject } = rxjs.webSocket;
 
   // the div containing the search suggestion results
   const suggestions = document.querySelector('#suggestions');
@@ -23,10 +26,48 @@
             be active for the life of this application.
   */
 
+  fromEvent(q, 'input')
+    .pipe(
+      debounceTime(300),
+      switchMap(() => {
+        return ajax.getJSON(getSearchURL(q.value))
+      })
+    ).subscribe(showSuggestions)
+
   // TODO: setup a WebSocketSubject
+  const socket = new WebSocketSubject('ws://localhost:8080');
+
+  const retryTime = (time) => retryWhen(switchMap(() => timer(time)));
 
   function getTickerStream(symbol) {
-    // TODO: multiplex the web socket (then add retry logic)
+    return socket.multiplex(
+      () => ({ type: 'sub', symbol }),
+      () => ({ type: 'unsub', symbol }),
+      v => v.symbol === symbol
+    )
+    .pipe(
+      map(v => v.price),
+      retryTime(1000)
+    );
+
+    // return new Observable(subscriber => {
+    //   socket.next({ type: 'sub', symbol });
+
+    //   console.log('dmsakl');
+    //   const subscription = socket.pipe(
+    //     filter(v => v.symbol === symbol)
+    //   ).subscribe(subscriber);
+
+    //   return () => {
+    //     console.log('dada');
+    //     socket.next({ type: 'unsub', symbol });
+    //     subscription.unsubscribe();
+    //   }
+    // })
+    // .pipe(
+    //   map(v => v.price),
+    //   retryTime(1000)
+    // );
   };
 
   // ***************************************************************************
