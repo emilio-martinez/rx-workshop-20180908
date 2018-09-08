3442e5365b5e3dd058098ffbee1c66ce9867541c
Emilio Martinez <e.marticor@gmail.com>
2018-09-08 09:08:13 -0700

    feat: exercise 04

--- exercises/node/4-subscribe-with-forEach.ts
+++ exercises/node/4-subscribe-with-forEach.ts
@@ -3,6 +3,17 @@ import { data$ } from './fixtures/1-data';
 
 // TODO: Subscribe to `data$` and log out all values to console.
 
+(async () => {
+  console.log('START');
+
+  await data$.forEach(v => console.log(v))
+    .then(
+      () => console.log('done'),
+      err => console.error(err)
+    );
+
+  console.log('START');
+})();
 
 // NOTE: If `forEach` returns a promise, how an we unsubscribe?
 //   We can't (yet! perhaps in the future of Rx?)
