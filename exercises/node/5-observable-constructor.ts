a7a6fff69291e8dbb30ea2ad10151090b3884b69
Emilio Martinez <e.marticor@gmail.com>
2018-09-08 09:18:45 -0700

    feat: exercise 05

--- exercises/node/5-observable-constructor.ts
+++ exercises/node/5-observable-constructor.ts
@@ -2,10 +2,17 @@ import { Observable } from 'rxjs';
 
 // TODO: create an observable with the Observable constructor that
 // emits the values 1, 2, 3 and completes.
-const source$; //<-- set me!
+const source$ = new Observable(subscriber => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  subscriber.complete();
+});
 
+console.log('START');
 source$.subscribe(
   x => console.log(x),
   err => console.error(err),
   () => console.info('done')
 );
+console.log('END');
